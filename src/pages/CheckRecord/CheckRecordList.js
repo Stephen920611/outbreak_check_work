9f77d360c065d1d190f200fec0bc5b7fcca244d8
stephen <13105208266@163.com>
2020-02-14 11:18:06 +0800

    导出功能数字bug修复

--- src/pages/CheckRecord/CheckRecordList.js
+++ src/pages/CheckRecord/CheckRecordList.js
@@ -645,7 +645,7 @@ class CheckRecordList extends PureComponent {
         let currentNum = Number(startPageNum);
         this.setState({
             // endPageNum: currentNum > 0 ? currentNum > (currentNum + maxPageSize) ? (currentNum + maxPageSize) : total : ''
-            endPageNum: (currentNum + maxPageSize) > total ? total : (currentNum + maxPageSize)
+            endPageNum: currentNum > 0 ? (currentNum + maxPageSize) > total ? total : (currentNum + maxPageSize) : ''
         })
         // console.log(currentNum,'currentNum');
         // let startNum;
