65cb8897b07acd26badd5cc09fb1d639eae94e64
stephen <13105208266@163.com>
2020-02-25 16:38:00 +0800

    新公共api地址

--- src/config/ENV.js
+++ src/config/ENV.js
@@ -5,44 +5,43 @@
  * @license dongfangdianzi
  */
 
-window.ENV = (function () {
-    let rootPath = '/pw/';     // 路由的根路径
-    // let apiDomain = 'http://192.168.10.122:8080/datadist/a/';    // api请求接口
-    // let apiDomain = 'http://56.82.130.19:8080/datadist/a/';    // 公安网
-    // let apiDomain = 'http://117.51.140.147:8080/datadist/a/';    //线上api接口
-    // let apiDomain = "http://192.168.2.100:8081/api"; //测试api接口
-    // let apiDomain = "http://49.232.51.60:8081/api"; //测试api接口
-    // let apiDomain = "http://61.156.14.19:10002/api"; //线上api接口
-    let apiDomain = "http://61.156.14.61:10005/api"; //线上api接口
+window.ENV = (function() {
+  let rootPath = '/pw/'; // 路由的根路径
+  // let apiDomain = 'http://192.168.10.122:8080/datadist/a/';    // api请求接口
+  // let apiDomain = 'http://56.82.130.19:8080/datadist/a/';    // 公安网
+  // let apiDomain = 'http://117.51.140.147:8080/datadist/a/';    //线上api接口
+  // let apiDomain = "http://192.168.2.100:8081/api"; //测试api接口
+  // let apiDomain = "http://49.232.51.60:8081/api"; //测试api接口
+  // let apiDomain = "http://61.156.14.19:10002/api"; //线上api接口
+  let apiDomain = 'http://61.156.14.61:10005/api'; //线上api接口
+  // let apiDomain = "http://49.232.51.60:9098/api"; //最后提供的正式api接口（打包用）
 
-    return {
-        apiDomain: apiDomain,         // api请求接口   测试服务器
-        rootPath: rootPath,                       	// 路由的根路径
-        apiSuccessCode: 0,                          // API接口响应成功的code
-        width: 1920,
-        height: 1080,
-        isAntdProRequest: false,        //是否是antd pro原来的请求方式，true需要加上api
+  return {
+    apiDomain: apiDomain, // api请求接口   测试服务器
+    rootPath: rootPath, // 路由的根路径
+    apiSuccessCode: 0, // API接口响应成功的code
+    width: 1920,
+    height: 1080,
+    isAntdProRequest: false, //是否是antd pro原来的请求方式，true需要加上api
 
-        login: {
-            loginInfoKey : "__login_info__",
-            errorCode: 900,                                 // 未登录的error code
-            isCheckLogin: false,                            // web端是否验证登录
-            cookieKey: '__login_solution_user_info__',               // 登录成功的cookie key, 用于验证当前页面是否登录
-            defaultRedirectUrl: rootPath + 'overview/list',  // 登录成功默认重定向的url
-            loginUrl: rootPath + 'login',                   // 登录页面url
+    login: {
+      loginInfoKey: '__login_info__',
+      errorCode: 900, // 未登录的error code
+      isCheckLogin: false, // web端是否验证登录
+      cookieKey: '__login_solution_user_info__', // 登录成功的cookie key, 用于验证当前页面是否登录
+      defaultRedirectUrl: rootPath + 'overview/list', // 登录成功默认重定向的url
+      loginUrl: rootPath + 'login', // 登录页面url
 
-            // 不需要验证是否登录的路由配置
-            noCheckIsLoginRoutes: [
-                rootPath + 'login',
-            ],
-        },
+      // 不需要验证是否登录的路由配置
+      noCheckIsLoginRoutes: [rootPath + 'login'],
+    },
 
-        mock: {
-            apiDomain: 'http://localhost:8180',     // mockApi请求接口
-            isStart: false,                         // 是否开启mock
-        },
-        logoTitle: '国家信息中心',                // logo标题文字
-        isShowLogoIcon: false,                      // 是否显示logo图标
-        isShowLoginCopy: false,
-    };
+    mock: {
+      apiDomain: 'http://localhost:8180', // mockApi请求接口
+      isStart: false, // 是否开启mock
+    },
+    logoTitle: '国家信息中心', // logo标题文字
+    isShowLogoIcon: false, // 是否显示logo图标
+    isShowLoginCopy: false,
+  };
 })();
